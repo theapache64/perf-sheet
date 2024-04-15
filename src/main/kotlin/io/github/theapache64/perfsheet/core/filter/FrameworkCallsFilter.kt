8f5b506f7ff6a3e358be323f3ab5227318b70173
theapache64 <theapache64@gmail.com>
2024-04-16 01:12:29 +0530

    👕 code cleanup

--- src/main/kotlin/io/github/theapache64/perfsheet/core/filter/FrameworkCallsFilter.kt
+++ src/main/kotlin/io/github/theapache64/perfsheet/core/filter/FrameworkCallsFilter.kt
@@ -2,7 +2,7 @@ package io.github.theapache64.perfsheet.core.filter
 
 class FrameworkCallsFilter : Filter() {
     companion object {
-        private val systemCallsRegex = listOf(
+        private val blackList = listOf(
             "androidx.compose.",
             "android.",
             "com.android.internal.",
@@ -24,12 +24,11 @@ class FrameworkCallsFilter : Filter() {
             "Record View#draw\\(\\)"
         ).joinToString(separator = "|", prefix = "^(", postfix = ").*").toRegex()
 
-        private val specialSystemCallsRegex = listOf(
+        private val whiteList = listOf(
             "android.app.ActivityThread.handleBindApplication",
             "android.app.ActivityThread.installContentProviders",
             "android.app.Activity.perform", // this will match onCreate, onStart, onResume, etc
             "androidx.lifecycle.ViewModelProvider.get", // viewModel query time
-
         ).joinToString(separator = "|", prefix = "^(", postfix = ").*")
             .also {
                 println("QuickTag: FrameworkCallsFilter:'$it': ")
@@ -38,7 +37,7 @@ class FrameworkCallsFilter : Filter() {
     }
 
     override fun apply(methodName: String): String? {
-        val isMatched = systemCallsRegex.matches(methodName) && !specialSystemCallsRegex.matches(methodName)
-        return if (isMatched) null else methodName
+        val shouldSkipMethodName = blackList.matches(methodName) && !whiteList.matches(methodName)
+        return if (shouldSkipMethodName) null else methodName
     }
 }
