38cc5c16fedf5c7d34977e52d1e332e3e7a49f51
theapache64 <theapache64@gmail.com>
2024-05-06 07:30:44 +0530

    🎉 bump version

--- src/main/kotlin/io/github/theapache64/perfsheet/ui/splash/SplashViewModel.kt
+++ src/main/kotlin/io/github/theapache64/perfsheet/ui/splash/SplashViewModel.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 
 class SplashViewModel @Inject constructor() {
     companion object {
-        private const val VERSION = "0.0.7"
+        private const val VERSION = "0.0.8"
     }
 
     private val _welcomeMsg = MutableLiveData<String>()
